1eedf87d3e9ca72f646281b905ac18c61722fddf
Prakhar Srivastava <50711734+prakhar-srivastava-1@users.noreply.github.com>
2022-02-26 04:30:22 +0530

    Complete code //Create a react app from scratch. //It should display 2 paragraph HTML elements. //The paragraphs should say: //Created by YOURNAME. //Copyright CURRENTYEAR.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+ReactDOM.render(
+  <div>
+    <h1>Hello there!</h1>
+    <p>Welcome to this hideous website!</p>
+    <p>Just playing around with React ;P</p>
+    <div>
+      Created by: Prakhar
+      <br />
+      &copy Copyright {new Date().getFullYear()}
+    </div>
+  </div>,
+  document.getElementById("root")
+);
